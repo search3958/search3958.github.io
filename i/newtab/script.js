f7231bd4f28170d3d72f3b11b95497aa71dc7eff
search3958 <157264638+search3958@users.noreply.github.com>
2024-10-24 20:00:36 +0900

    Update script.js

--- i/newtab/script.js
+++ i/newtab/script.js
@@ -9,8 +9,11 @@ links.forEach(link => {
         event.preventDefault(); // デフォルトのリンク動作を防ぐ
         const href = this.getAttribute('href'); // クリックしたリンクのhrefを取得
         const section = new URLSearchParams(href.split('?')[1]).get('section'); // section パラメータを取得
+        
+        // sectionが取得できた場合、iframeのsrcを更新
         if (section) {
-            updateFrame(section); // iframeのsrcを更新
+            const contentUrl = `https://search3958.github.io/i/newtab/content${section}`; // ここでURLを作成
+            updateFrame(contentUrl); // iframeのsrcを更新
             updateURL(href); // URLを更新
         }
     });
@@ -32,7 +35,8 @@ function updateContentFromURL() {
     const section = params.get('section');
 
     if (section) {
-        updateFrame(section); // iframeのsrcを更新
+        const contentUrl = `https://search3958.github.io/i/newtab/content${section}`; // URLを作成
+        updateFrame(contentUrl); // iframeのsrcを更新
     }
 }
 
